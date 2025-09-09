ee720a3b6f340aeedb747821c270fe7b09b3374e
Prathmesh Kolte <kolteprathamesh1905@gmail.com>
2025-09-09 21:28:46 +0000

    i have added api kehys but it couldnt fetch chatbot data from envitonmen

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,7 +1,10 @@
 import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/googleai';
+import {config} from 'dotenv';
+
+config();
 
 export const ai = genkit({
-  plugins: [googleAI()],
-  model: 'googleai/gemini-2.5-flash',
+  plugins: [googleAI({apiKey: process.env.GEMINI_API_KEY})],
+  model: 'googleai/gemini-1.5-flash',
 });
