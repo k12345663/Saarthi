1829220eff747eeae19f4afcbb975d37d95e3fb7
Prathmesh Kolte <kolteprathamesh1905@gmail.com>
2025-09-09 21:31:07 +0000

    gemini and open ai both

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -5,6 +5,8 @@ import {config} from 'dotenv';
 config();
 
 export const ai = genkit({
-  plugins: [googleAI({apiKey: process.env.GEMINI_API_KEY})],
+  plugins: [
+    googleAI({apiKey: process.env.GEMINI_API_KEY}),
+  ],
   model: 'googleai/gemini-1.5-flash',
 });
